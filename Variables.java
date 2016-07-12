c11836de34ef0171b33432e423bf080491f41c2a
Wolfie Davis <wolfiedavis@gmail.com>
2016-07-12 13:25:11 -0400

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Learning variables!");
+        
+        int num = 22;
+        System.out.println(num);
+    }
+}
