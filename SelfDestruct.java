9a997baed2bcf5ed9b8e8116229f23d0f332eeb7
Wolfie Davis <wolfiedavis@gmail.com>
2016-07-12 13:49:44 -0400

    Create SelfDestruct.java

---
+++ SelfDestruct.java
@@ -0,0 +1,110 @@
+public class SelfDestruct {
+    public static void main (String[] args) {
+        
+    System.out.println("Self Destruct in");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("10");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("9");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("8");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("7");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("6");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("5");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("4");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("3");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("2");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("1");
+    
+    try {
+    Thread.sleep(2000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+    
+    System.out.println("0");
+    
+    try {
+    Thread.sleep(4000);
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+
+    System.out.println("KABOOM!");
+    
+
+    }
+}
+
+
+
+
+
+
